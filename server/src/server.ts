a453e064801a6e572d645b4c9865a4b658ca4f65
Felipe <felipekun213@gmail.com>
2020-08-12 21:43:51 -0300

    dia 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes)
+app.use(cors())
 
 // Rotes
 //localhost:3333/users

3cde79ecdc28ca5be9e0e9814d16ead4e3bd938c
Felipe <felipekun213@gmail.com>
2020-08-17 17:04:44 -0300

    Versão Mobile

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -15,8 +15,8 @@ const Select: React.FC<SelectProps> = ({label, name, options, ...rest}) => {
     return (
         <div className="select-block">
         <label htmlFor={name}>{label}</label>
-        <select id={name} value={'DEFAULT'} {...rest} >
-            <option value="DEFAULT" disabled hidden>Selecione uma opção</option>
+        <select id={name} value={""} {...rest} >
+            <option value="" disabled hidden>Selecione uma opção</option>
             {options.map(option => {
                 return <option key={option.value} value={option.value} label={option.label}></option>
             })}
